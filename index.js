ad5924e353411622833f1cf1cf224c8b36ce6d01
ruks21 <rukioismail98@gmail.com>
2023-06-01 15:06:56 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let x=15;
+console.log(x);
+
+let a=7;
+console.log(a);
+
+let names=["jim", "james", "paul", "tyrone"];
+console.log(names); 
+
+let seed1="carrots";
+console.log(seed1);
+
+let vegetables=20;
+console.log(vegetables);
