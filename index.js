ba09eb06120d2e312a35c469e22c3dd141270e5c
ruks21 <rukioismail98@gmail.com>
2023-06-02 14:55:30 -0400

    if statements

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
-let x=15;
-console.log(x);
+ // let x=15;
+ // console.log(x);
 
-let a=7;
-console.log(a);
+ // let a=7;
+ // console.log(a);
 
-let names=["jim", "james", "paul", "tyrone"];
-console.log(names); 
+ // let names=["jim", "james", "paul", "tyrone"];
+ // console.log(names); 
 
-let seed1="carrots";
-console.log(seed1);
+ // let seed1="carrots";
+ // console.log(seed1);
 
-let vegetables=20;
-console.log(vegetables);
+ // let vegetables=20;
+ // console.log(vegetables);
+
+// let x=12;
+// let a=7;
+
+// if (x === 12 && y === 7)
+// { let sum=x+a;
+// console.log(sum);
+// }
+
+// let username='admin';
+// let password='Pass614';
+// if (username ==='admin' && password ==='Pass614'){
+//     //do this 
+//     console.log("access granted");
+// }
+
+// else{
+//     //do this
+//     console.log("access denied");
+// }
+
+let score=40;
+
+if ( score>= 80)
+{console.log("You did a good job!");
+}
+
+else if (score >= 70)
+{console.log("Keep trying");
+
+}
+
+else if (score >= 60)
+{console.log("ehh");
+
+}
+
+else {
+    (score < 55)
+    console.log("Bad grade but you'll get em next time")
+}
